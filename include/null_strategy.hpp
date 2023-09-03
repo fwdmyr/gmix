3a3b6fc5763ad0d70f32c982b4961c216855cd12
felix <ftwidmayer@gmail.com>
2023-09-03 09:40:39 +0200

    Made null strategy truly null

--- include/null_strategy.hpp
+++ include/null_strategy.hpp
@@ -1,31 +1,11 @@
 #ifndef GMSAM_NULL_STRATEGY_HPP
 #define GMSAM_NULL_STRATEGY_HPP
 
-#include "gaussian_component.hpp"
-#include <stdexcept>
-#include <vector>
-
 namespace gmix {
-
-struct NullStrategyUsedException : public std::runtime_error {
-  NullStrategyUsedException() noexcept
-      : std::runtime_error("Tried to use NullStrategy"){};
-};
 
 template <int Dim> class NullStrategy {
 protected:
   NullStrategy() = default;
-
-public:
-  void fit(std::vector<GaussianComponent<Dim>> &,
-           const StaticRowsMatrix<Dim> &) const {
-    throw NullStrategyUsedException{};
-  }
-
-  void initialize(std::vector<GaussianComponent<Dim>> &,
-                  const StaticRowsMatrix<Dim> &) const {
-    throw NullStrategyUsedException{};
-  }
 };
 
 } // namespace gmix
